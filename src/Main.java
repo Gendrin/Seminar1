a451286122841368da03793a1046b89a1e442ef3
Gendrin <Gendrinvl@gmail.com>
2022-11-26 02:31:29 +1000

    Task1 commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+
 public class Main {
     //Test module
     //Second commit
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       // System.out.println("Hello world!");
+
     }
+    public static class Task1 {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            for (int i = m+n-1, a = m-1, b = n-1; b>=0; i--) {
+                if (a >= 0 && nums1[a] > nums2[b]) nums1[i] = nums1[a--];
+                else nums1[i] = nums2[b--];
+            }
+
+        }
+    }
+
+
 }
