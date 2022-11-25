0b25845c8ada75a57cd39e392165506cd61a0b9f
Gendrin <Gendrinvl@gmail.com>
2022-11-26 02:34:53 +1000

    Task2 commit

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,14 @@ public class Main {
         }
     }
 
+    class Task2 {
+        public int[] buildArray(int[] nums) {
+            var ans = new int[nums.length];
+            for (var i = 0; i < nums.length; i++)
+                ans[i] = nums[nums[i]];
+            return ans;
+        }
+    }
+
 
 }
